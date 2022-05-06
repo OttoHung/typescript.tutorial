a5f699b50c025cef2cab3e02bbabfe53ada008f2
OttoHung <totsi04@gmail.com>
2022-05-06 14:11:28 +1000

    Add hash table

---
+++ workspaces/interview/src/hash_table.ts
@@ -0,0 +1,26 @@
+enum ModuloType {    
+    DoubleHashing,
+    LinearProbing,
+    PrimeSum,
+    QuatraticProbing,    
+}
+
+export interface HashTable {
+    table: any[]
+}
+
+const getModulo = (type: ModuloType): number => {
+
+}
+
+export const insertData = (data: any, table?: HashTable): HashTable => {
+    
+}
+
+export const searchData = (): HashTable => {
+
+}
+
+export const deleteData = (): HashTable => {
+
+}
